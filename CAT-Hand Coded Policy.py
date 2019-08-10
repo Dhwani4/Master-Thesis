725669be167e9084837beb089e5dbdd3546348c4
Dhwani4 <32913303+Dhwani4@users.noreply.github.com>
2019-08-10 14:41:53 -0400

    Update CAT-Hand Coded Policy.py

--- CAT-Hand Coded Policy.py
+++ CAT-Hand Coded Policy.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Aug  8 15:08:48 2019
-
-@author: Dhwani
-"""
+# Calling Dijkstra's algorithm to find shortest path using code from https://www.bogotobogo.com/python/python_Dijkstras_Shortest_Path_Algorithm.php
 import numpy as np
 import heapq
 import matplotlib.pyplot as pt
